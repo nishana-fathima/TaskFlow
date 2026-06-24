056adff706a76a83060540509d0500f7f6366cfd
nishanafathima-dev <nishanaf731@gmail.com>
2026-06-24 16:22:10 +0530

    feat(authentication done)

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,6 +1,8 @@
 import db from "../config/db.js";
 import bcrypt from "bcryptjs";
 
+
+//Register a new user 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -44,6 +46,58 @@ export const register = async (req, res) => {
           });
         }
       );
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+//Login a user
+export const login = (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    const query = "SELECT * FROM users WHERE email = ?";
+
+    db.query(query, [email], async (err, result) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      if (result.length === 0) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
+
+      const user = result[0];
+
+      const isMatch = await bcrypt.compare(
+        password,
+        user.password
+      );
+
+      if (!isMatch) {
+        return res.status(401).json({
+          message: "Invalid credentials",
+        });
+      }
+
+      res.status(200).json({
+        message: "Login successful",
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        },
+      });
     });
   } catch (error) {
     res.status(500).json(error);
