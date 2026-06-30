e0cf4df0e2177f7b1fb1a9d7dc28002866a3b601
nishanafathima-dev <nishanaf731@gmail.com>
2026-06-30 15:49:48 +0530

    feat(no changes)

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/dashboard", dashboardRoutes);
+// app.use("/api/dashboard", dashboardRoutes);
 
 export default app;
